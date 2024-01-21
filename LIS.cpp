65b24608196193f7fc87b2c3e878336472de5e36
Kh Sadman Sakib <119437906+sakibsidha@users.noreply.github.com>
2024-01-21 20:32:25 +0600

    Create LIS.cpp

---
+++ LIS.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+	long long n; cin >> n;
+	vector<long long> v(n), dp(n, 0);
+	for(int i = 0; i < n; ++i){
+		cin >> v[i];
+	}
+	vector<long long> next(n, -1);
+	for(int i = n-1; i >= 0; --i){
+		long long mx = 0;
+		for(int j = i+1; j < n; ++j){
+			if(v[j] > v[i]){
+				if(dp[j] > mx){
+					next[i] = j;
+					mx = dp[j];
+				}
+			}
+		}
+		dp[i] = 1 + mx;
+	}
+	long long ans = 0, start = -1;
+	for(int i = 0; i < n; ++i){
+		if(dp[i] > ans){
+			start = i;
+			ans = dp[i];
+		}
+	}
+	cout << "Length: " << ans << endl;
+	vector<long long> lis;
+	while(start != -1){
+		lis.push_back(v[start]);
+		start = next[start];
+	}
+	cout << "LIS: ";
+	for(auto it: lis) cout << it << ' ';
+	cout << '\n';
+}
+
+int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	//int tc; cin >> tc;
+	//while(tc--)
+		solve();
+	
+	return 0;
+}

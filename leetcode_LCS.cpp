373d53a56fb292da5c7769ae0a9a264eb9e92d4c
Kh Sadman Sakib <119437906+sakibsidha@users.noreply.github.com>
2024-01-21 21:51:11 +0600

    Update leetcode_LCS.cpp

--- leetcode_LCS.cpp
+++ leetcode_LCS.cpp
@@ -31,3 +31,22 @@ public:
         return f(0,0);
     }
 };
+
+// iterative:
+// class Solution {
+// public:
+//     int dp[1001][1001];
+//     int longestCommonSubsequence(string text1, string text2) {
+//         int n = text1.size();
+//         int m = text2.size();
+//         for(int i = n-1; i >= 0; --i){
+//             for(int j = m-1; j >= 0; --j){
+//                 if(text1[i]==text2[j]) dp[i][j] = 1 + dp[i+1][j+1];
+//                 else{
+//                     dp[i][j] = max(dp[i+1][j],dp[i][j+1]);
+//                 }
+//             }
+//         }
+//         return dp[0][0];
+//     }
+// };

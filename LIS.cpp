2f1859925e0af7c5ffbe58b2343cadba521a5d27
Kh Sadman Sakib <119437906+sakibsidha@users.noreply.github.com>
2024-01-21 20:34:38 +0600

    Update LIS.cpp

--- LIS.cpp
+++ LIS.cpp
@@ -7,13 +7,15 @@ void solve(){
 	for(int i = 0; i < n; ++i){
 		cin >> v[i];
 	}
+	// state: dp[i] is the LIS starting from idx i
+	// transition: dp[i] = 1 + max(dp[j]) where i < j < n
 	vector<long long> next(n, -1);
 	for(int i = n-1; i >= 0; --i){
 		long long mx = 0;
 		for(int j = i+1; j < n; ++j){
 			if(v[j] > v[i]){
 				if(dp[j] > mx){
-					next[i] = j;
+					next[i] = j; // when we get a better ans, we keep track of the path
 					mx = dp[j];
 				}
 			}

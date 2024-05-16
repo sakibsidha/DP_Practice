c5a7a023b58bd94e2b9b25dfb0ccb6998c4808dc
Kh Sadman Sakib <119437906+sakibsidha@users.noreply.github.com>
2024-05-16 21:47:56 +0600

    Update leetcode_Edit_Distance.cpp

--- leetcode_Edit_Distance.cpp
+++ leetcode_Edit_Distance.cpp
@@ -1,37 +1,17 @@
 class Solution {
 public:
-    string s, t;
-    int n, m;
-    int dp[501][501];
-    int f(int i, int j, int n, int m) {
-        if (i == n || j == m) {
-            if (i == n)
-                return (m - j);
-            else
-                return (n - i);
-        }
-        if (dp[i][j] != -1)
-            return dp[i][j];
-        if (s[i] == t[j]) {
-            int a = f(i + 1, j + 1, n, m); // no change
-            int b = 1 + f(i + 1, j + 1, n, m); // add
-            int c = 1 + f(i + 1, j, n, m); // remove
-            int d = 1 + f(i, j + 1, n, m); // replace
-            dp[i][j] = min(a, min(b, min(c, d)));
-        } else {
-            int b = 1 + f(i + 1, j + 1, n, m);
-            int c = 1 + f(i + 1, j, n, m);
-            int d = 1 + f(i, j + 1, n, m);
-            dp[i][j] = min(b, min(c, d));
-        }
-        return dp[i][j];
-    }
     int minDistance(string word1, string word2) {
-        n = word1.size();
-        m = word2.size();
-        s = word1;
-        t = word2;
-        memset(dp, -1, sizeof(dp));
-        return f(0, 0, n, m);
+        int n = word1.size();
+        int m = word2.size();
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+        function<int(int,int)> f = [&](int i, int j){
+            if(i < 0 && j < 0) return 0;
+            if(i < 0) return j + 1;
+            if(j < 0) return i + 1;
+            if(dp[i][j] != -1) return dp[i][j];
+            if(word1[i] == word2[j]) return dp[i][j] = f(i-1, j-1);
+            else return dp[i][j] = std::min(1 + f(i-1, j), std::min(1 + f(i, j-1), 1 + f(i-1, j-1)));
+        }; 
+        return f(n-1, m-1);
     }
 };
